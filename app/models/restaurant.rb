ab54c05551c5b4157ee63e247cf37af31676dab3
ivan schlagheck <ivansch05@gmail.com>
2021-11-10 15:50:04 +0100

    finished but the style is horrible

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,5 +3,6 @@ class Restaurant < ApplicationRecord
 
   validates :name, presence: true
   validates :address, presence: true
+  validates :phone_number, presence: true
   validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
 end
